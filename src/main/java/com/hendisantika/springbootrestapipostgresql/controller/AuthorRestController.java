95bf2f73406599e2bf559448ed14f4a38a1b3bdd
Adela Merko <merkoadela27@gmail.com>
2024-04-17 12:47:02 +0200

    Added more logs using Log4j

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
@@ -23,45 +23,105 @@ public class AuthorRestController {
 
     @PostMapping
     public ResponseEntity<?> addAuthor(@RequestBody Author author) {
-        logger.info("Add author {}", author);
-        return new ResponseEntity<>(repository.save(author), HttpStatus.CREATED);
+        logger.info("Adding author: {}", author);
+        try {
+            Author savedAuthor = repository.save(author);
+            logger.info("Author added successfully: {}", savedAuthor);
+            return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
+        } catch (Exception e) {
+            logger.error("Error occurred while adding author", e);
+            return new ResponseEntity<>("Failed to add author", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping
     public ResponseEntity<Collection<Author>> getAllAuthors() {
-        logger.info("Get all authors");
-        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+        logger.info("Retrieving all authors");
+        try {
+            Collection<Author> authors = repository.findAll();
+            logger.info("Found {} authors", authors.size());
+            return new ResponseEntity<>(authors, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error occurred while retrieving authors", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
-        logger.info("Get author with id {}", id);
-        return new ResponseEntity<>(repository.findById(id).get(), HttpStatus.OK);
+        logger.info("Retrieving author with id: {}", id);
+        try {
+            Author author = repository.findById(id).orElse(null);
+            if (author != null) {
+                logger.info("Author found: {}", author);
+                return new ResponseEntity<>(author, HttpStatus.OK);
+            } else {
+                logger.warn("Author not found with id: {}", id);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while retrieving author with id: {}", id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(params = {"name"})
     public ResponseEntity<Collection<Author>> findAuthorWithName(@RequestParam(value = "name") String name) {
-        logger.info("Get author by name", name);
-        return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
+        logger.info("Retrieving author by name: {}", name);
+        try {
+            Collection<Author> authors = repository.findByName(name);
+            logger.info("Found {} authors with name: {}", authors.size(), name);
+            return new ResponseEntity<>(authors, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error occurred while retrieving author with name: {}", name, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Author> updateAuthor(@PathVariable("id") long id, @RequestBody Author author) {
-        Optional<Author> currentAuthorOpt = repository.findById(id);
-        Author currentAuthor = currentAuthorOpt.get();
-        currentAuthor.setName(author.getName());
-        return new ResponseEntity<>(repository.save(currentAuthor), HttpStatus.OK);
+        logger.info("Updating author with id: {}", id);
+        try {
+            Optional<Author> currentAuthorOpt = repository.findById(id);
+            if (currentAuthorOpt.isPresent()) {
+                Author currentAuthor = currentAuthorOpt.get();
+                currentAuthor.setName(author.getName());
+                Author updatedAuthor = repository.save(currentAuthor);
+                logger.info("Author updated successfully: {}", updatedAuthor);
+                return new ResponseEntity<>(updatedAuthor, HttpStatus.OK);
+            } else {
+                logger.warn("Author not found with id: {}", id);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while updating author with id: {}", id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAuthorById(@PathVariable Long id) {
-        logger.info("Delete author with id", id);
-        repository.deleteById(id);
+    public ResponseEntity<Void> deleteAuthorById(@PathVariable Long id) {
+        logger.info("Deleting author with id: {}", id);
+        try {
+            repository.deleteById(id);
+            logger.info("Author deleted successfully with id: {}", id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            logger.error("Error occurred while deleting author with id: {}", id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping
-    public void deleteAllAuthors() {
+    public ResponseEntity<Void> deleteAllAuthors() {
         logger.info("Deleting all authors");
-        repository.deleteAll();
+        try {
+            repository.deleteAll();
+            logger.info("All authors deleted successfully");
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            logger.error("Error occurred while deleting all authors", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
